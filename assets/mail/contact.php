e4d2553bdd7d4fc63827541890e94a9013aeb73c
Margav <alonebhayankar@gmail.com>
2026-09-05 22:29:19 +0530

    update-12

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -1,85 +1,178 @@
 <?php
 
-if(!$_POST) exit;
-
-// Email address verification, do not edit.
-function isEmail($email) {
-	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
-}
-
-if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
-
-$name     = $_POST['name'];
-$email    = $_POST['email'];
-$phone     = $_POST['phone'];
-$comments = $_POST['comments'];
-
-if(trim($name) == '') {
-	echo '<div class="alert alert-error">You must enter your name.</div>';
-	exit();
-} else if(trim($email) == '') {
-	echo '<div class="alert alert-error">You must enter email address.</div>';
-	exit();
-} else if(!isEmail($email)) {
-	echo '<div class="alert alert-error">You must enter a valid email address.</div>';
-	exit();
-} else if(trim($phone) == '') {
-	echo '<div class="alert alert-error">Please fill all fields!</div>';
-	exit();
-}
-else if(trim($comments) == '') {
-	echo '<div class="alert alert-error">You must enter your comments</div>';
-	exit();
-}
-
-if(get_magic_quotes_gpc()) {
-	$comments = stripslashes($comments);
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    exit('Invalid request.');
 }
 
 
-// Configuration option.
-// Enter the email address that you want to emails to be sent to.
-// Example $address = "joe.doe@yourdomain.com";
+/* ==============================
+   GET FORM DATA
+   ============================== */
 
-//$address = "example@themeforest.net";
+$name = trim($_POST['name'] ?? '');
+$email = trim($_POST['email'] ?? '');
+$phone = trim($_POST['phone'] ?? '');
+$code = trim($_POST['code'] ?? '');
+$comments = trim($_POST['comments'] ?? '');
+
+$serviceFor = trim($_POST['service_for'] ?? '');
+$frequency = trim($_POST['frequency'] ?? '');
+
+$services = $_POST['services'] ?? [];
+
+
+/* ==============================
+   VALIDATION
+   ============================== */
+
+if ($name === '') {
+    exit('<div class="alert alert-danger">Please enter your name.</div>');
+}
+
+if ($email === '') {
+    exit('<div class="alert alert-danger">Please enter your email address.</div>');
+}
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    exit('<div class="alert alert-danger">Please enter a valid email address.</div>');
+}
+
+if ($phone === '') {
+    exit('<div class="alert alert-danger">Please enter your phone number.</div>');
+}
+
+if ($code === '') {
+    exit('<div class="alert alert-danger">Please enter your postal code.</div>');
+}
+
+if ($serviceFor === '') {
+    exit('<div class="alert alert-danger">Please select Residential or Commercial.</div>');
+}
+
+if ($frequency === '') {
+    exit('<div class="alert alert-danger">Please select a cleaning frequency.</div>');
+}
+
+if (empty($services)) {
+    exit('<div class="alert alert-danger">Please select at least one service.</div>');
+}
+
+
+/* ==============================
+   SERVICE TYPE
+   ============================== */
+
+if ($serviceFor === '1') {
+    $serviceForText = 'Residential';
+} elseif ($serviceFor === '2') {
+    $serviceForText = 'Commercial';
+} else {
+    $serviceForText = 'Unknown';
+}
+
+
+/* ==============================
+   CLEANING FREQUENCY
+   ============================== */
+
+$frequencyNames = [
+    '1' => 'One-Time',
+    '2' => 'Weekly',
+    '3' => 'Bi-Weekly',
+    '4' => 'Monthly',
+    '5' => 'Quarterly'
+];
+
+$frequencyText = $frequencyNames[$frequency] ?? 'Unknown';
+
+
+/* ==============================
+   SERVICES
+   ============================== */
+
+$servicesText = implode(', ', $services);
+
+
+/* ==============================
+   EMAIL ADDRESS
+   ============================== */
+
+/*
+ * CHANGE THIS EMAIL ADDRESS
+ * to the email where you want
+ * to receive quote requests.
+ */
+
 $address = "Info@yourdomain.com";
 
 
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
+/* ==============================
+   EMAIL SUBJECT
+   ============================== */
 
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
-
-$e_subject = 'Contact Form';
+$subject = "New Free Quote Request";
 
 
-// Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
+/* ==============================
+   EMAIL MESSAGE
+   ============================== */
 
-$e_body = "You have been contacted by $name, their additional message is as follows." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $name via email, $email";
+$message  = "NEW FREE QUOTE REQUEST";
+$message .= PHP_EOL . PHP_EOL;
 
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+$message .= "Name: " . $name . PHP_EOL;
+$message .= "Email: " . $email . PHP_EOL;
+$message .= "Phone: " . $phone . PHP_EOL;
+$message .= "Postal Code: " . $code . PHP_EOL;
 
-$headers = "From: $email" . PHP_EOL;
-$headers .= "Reply-To: $email" . PHP_EOL;
+$message .= PHP_EOL;
+
+$message .= "Service For: " . $serviceForText . PHP_EOL;
+$message .= "Cleaning Frequency: " . $frequencyText . PHP_EOL;
+$message .= "Services Requested: " . $servicesText . PHP_EOL;
+
+$message .= PHP_EOL;
+
+$message .= "Additional Details:" . PHP_EOL;
+
+if ($comments !== '') {
+    $message .= $comments . PHP_EOL;
+} else {
+    $message .= "No additional details provided." . PHP_EOL;
+}
+
+$message .= PHP_EOL;
+$message .= "----------------------------------------" . PHP_EOL;
+$message .= "Submitted from website quote form." . PHP_EOL;
+
+
+/* ==============================
+   EMAIL HEADERS
+   ============================== */
+
+$headers  = "From: Website Quote Form <" . $address . ">" . PHP_EOL;
+$headers .= "Reply-To: " . $email . PHP_EOL;
 $headers .= "MIME-Version: 1.0" . PHP_EOL;
-$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+$headers .= "Content-Type: text/plain; charset=UTF-8" . PHP_EOL;
 
-if(mail($address, $e_subject, $msg, $headers)) {
 
-	// Email has sent successfully, echo a success page.
+/* ==============================
+   SEND EMAIL
+   ============================== */
 
-	echo "<div class='alert alert-success'>";
-	echo "<h3>Email Sent Successfully.</h3>";
-	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
-	echo "</div>";
+if (mail($address, $subject, $message, $headers)) {
+
+    echo '<div class="alert alert-success">';
+    echo '<h3>Request Sent Successfully!</h3>';
+    echo '<p>Thank you <strong>' . htmlspecialchars($name) . '</strong>, your quote request has been submitted.</p>';
+    echo '</div>';
 
 } else {
 
-	echo 'ERROR!';
-
+    echo '<div class="alert alert-danger">';
+    echo '<h3>Unable to Send Request</h3>';
+    echo '<p>Sorry, something went wrong while sending your request. Please try again.</p>';
+    echo '</div>';
 }
+
+?>
